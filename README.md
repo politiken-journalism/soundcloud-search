da5d2162c203ff871af23fea0b4f4c17ca69fafc
Emil Bay <github@tixz.dk>
2015-07-14 17:07:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+`soundcloud-search`
+===================
+
+CLI to query Soundcloud for tracks matching a given query
+
+Installation
+------------
+
+```bash
+npm install soundcloud-search
+```
+
+Add `-g` to install globally, otherwise execute using `node_modules/.bin/soundcloud-search`
+
+Usage
+-----
+
+```
+NAME
+  soundcloud-search - CLI to query Soundcloud for tracks matching a given query
+
+SYNOPSIS
+  soundcloud-search [OPTION]... FILE
+
+DESCRIPTION
+  Resolve track queries to Soundcloud tracks, taking queries as each line of FILE.
+  Unfortunately an explicit input file and output file has to be given since
+  STDIN and STDOUT are used to prompt the user to resolve tracks for each query.
+
+  -o, --output-file
+    required. Where to write resulting JSON objects. Output as NDJSON
+
+  -n, --num-results
+    max number of results to display when prompting the user to resolve queries
+
+  -s, --soundcloud-client-id
+    soundcloud client ID to use when accessing their API. Can also be set with
+    the env var SC_CLIENT_ID
+
+EXAMPLE
+
+  Read all track plays from a CSV file with headers `artist` and `track`
+
+    export SC_CLIENT_ID="..."
+    soundcloud-search -o results/tracks.ndjson \
+      <(cat data/plays.csv | \
+        csv-parser --seperator=";" | \
+        jq -r '.artist + " - " + .track' \
+      )
+```
+
+License
+-------
+
+[ISC](LICENSE)
---
+++ cli.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+'use strict'
+
+var Transform = require('readable-stream/transform')
+var concat = require('concat-stream')
+var split = require('split')
+var ndjson = require('ndjson')
+var fs = require('fs')
+var prompt = require('inquirer').prompt
+
+var createApiStream = require('./lib/api-stream')
+
+var argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    o: 'output-file',
+    n: 'num-results',
+    s: 'soundcloud-client-id'
+  }
+})
+
+var inputFile = argv._[0]
+var outputFile = argv.o
+var numResults = argv.n || 5
+var SC_CLIENT_ID = argv.scid || process.env.SC_CLIENT_ID
+
+if (!inputFile) {
+  console.error('Must provide input file. STDIN is reserved for answering questions')
+  process.exit(1)
+}
+
+if (!outputFile) {
+  console.error('Must provide output file. STDOUT is reserved for answering questions')
+  process.exit(1)
+}
+
+if (!SC_CLIENT_ID) {
+  console.error('Must provide Soundcloud Client ID')
+  process.exit(2)
+}
+
+var pendingQueries = []
+fs.createReadStream(inputFile)
+  .pipe(split())
+  .pipe(Transform({
+    objectMode: true,
+    transform (query, enc, cb) {
+      pendingQueries.push(query.toString('utf8'))
+      this.push({q: query.toString('utf8')})
+      cb()
+    }
+  }))
+  .pipe(createApiStream('https://api.soundcloud.com/tracks?client_id=' + SC_CLIENT_ID + '{&q}'))
+  .pipe(concatResponse(pendingQueries))
+  .pipe(listOptions())
+  .pipe(ndjson.serialize())
+  .pipe(fs.createWriteStream(outputFile))
+
+function concatResponse (queryQueue) {
+  return Transform({
+    objectMode: true,
+    highwaterMark: 1,
+    transform (req, enc, cb) {
+      var self = this
+      req.pipe(concat(function (body) {
+        var currentQuery = pendingQueries.shift()
+        try {
+          var json = JSON.parse(body)
+          json.query = currentQuery
+          self.push(json)
+          cb()
+        } catch (ex) {
+          cb(ex)
+        }
+      }))
+    }
+  })
+}
+
+function listOptions () {
+  return Transform({
+    objectMode: true,
+    transform (results, enc, cb) {
+      var self = this
+      prompt([{
+        type: 'list',
+        name: 'q',
+        message: results.query,
+        choices: results.slice(0, numResults).map(function (value) {
+          return {
+            name: value.title,
+            value
+          }
+        }).concat({name: 'Skip', value: null})
+      }], function (answers) {
+        if (answers.q) self.push(answers.q)
+        cb()
+      })
+    }
+  })
+}
